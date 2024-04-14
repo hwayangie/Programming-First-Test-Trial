e846407fd11d2b2d9c5eb7ed07ece0daa544c537
marchjeff145@gmail.com <marchjeff145@gmail.com>
2024-04-14 23:52:24 +0700

    update

---
+++ exercise/ex1.js
@@ -0,0 +1,51 @@
+// let name = 'lucy';    // tao ra mot bien chuoi voi gia tri lucy
+// let age = 0.5;
+// let isgirl = false;
+
+
+// if (name != 'lucy') {               // so sanh gia tri neu name khac voi 'lucy'
+//   console.log('toi la lucy');       // in ra 'toi la lucys'
+// } else {                            // neu sai thi
+//   console.log('toi la vang khe');   // in ra 'toi la vang khe'
+// }
+// if (age >= 0.5) {
+//   console.log('da truong thanh');
+
+// } else {
+//   console.log('chua truong thanh');
+// }
+
+let people = {
+  name: 'dhjhj',
+  age: 22
+
+};
+console.log(people.name);
+console.log(people.age);
+
+let masocccd = 1234567888888;
+
+console.log(masocccd);
+
+if (people.age > 18) {
+  console.log('ban da qua tuoi');
+} else if (people.age < 18) {
+  console.log('ban chua du tuoi');
+} else {
+  console.log('ban du tuoi');
+}
+
+// if (condition) {
+//   // if condition is true
+// } else {
+//   // if condition is false
+// }
+
+
+// if (condition2) {
+//   // if condition2 is true
+// } else if (condition3) {
+//   // if condition3 is true
+// } else {
+//   // if condition2 and condition3 is false
+// }
